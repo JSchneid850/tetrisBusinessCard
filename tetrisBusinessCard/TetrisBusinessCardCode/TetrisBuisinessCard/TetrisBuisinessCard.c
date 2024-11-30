e0e6c746de23ad4e06132f071c7e81747f30bc9a
Josh Schneider <joshschneider145@gmail.com>
2024-11-30 14:11:51 -0600

    vertical scanning example

--- tetrisBusinessCard/TetrisBusinessCardCode/TetrisBuisinessCard/TetrisBuisinessCard.c
+++ tetrisBusinessCard/TetrisBusinessCardCode/TetrisBuisinessCard/TetrisBuisinessCard.c
@@ -3,7 +3,7 @@
 #include "hardware/pio.h"
 const uint32_t GPIO_PIN_COUNT = 27;
 const uint32_t LED_PIN_COUNT = 22;
-const uint32_t LEDMask = 0x4FFFFF;
+const uint32_t LEDMask = 0x3FFFFF;
 const uint32_t GPIOMask =  0x2FFFFFF;
 
 uint calculate_hi_res_mask(uint lowMask, uint hiMask){
@@ -17,8 +17,31 @@ int main() {
     //turn on all pins needed
     gpio_init_mask(GPIOMask);
 
-    uint32_t LEDHiMask = 0b1;
-    uint32_t LEDLowMask = 0b10;
+    uint32_t LEDTargetBit = 0b1;
+    uint32_t targetMask;
+    //uint32_t dataIn = 0b001001001001;
+
+    while(true){
+         for(uint32_t i = 0; i<LED_PIN_COUNT; i++){
+             for(uint32_t j = 0; j<LED_PIN_COUNT; j++){
+                 if (i==j){
+                     j++;
+                 }
+                 targetMask = (LEDTargetBit<<i)|(LEDTargetBit<<j);
+                 gpio_set_dir_in_masked(LEDMask);
+                 gpio_set_dir_out_masked(targetMask);
+                 gpio_put_masked(targetMask, LEDTargetBit<<j);
+                sleep_ms(100);
+                printf("target mask: %x, HiVal: %x\n",targetMask,LEDTargetBit<<j);
+             }
+         }
+                // gpio_set_dir_in_masked(LEDMask);
+                // gpio_set_dir_out_masked(0b11);
+                // gpio_put_masked(0b11, 0b01);
+
+    }
+}
+
 
         // gpio_set_dir_out_masked((LEDHiMask<<i)&(LEDLowMask<<j));
         // gpio_set_dir_in_masked(calculate_hi_res_mask(LEDHiMask<<i, LEDLowMask<<j));
@@ -41,19 +64,3 @@ int main() {
         // gpio_set_dir(1, GPIO_OUT);
         // gpio_put(0, true);
         // gpio_put(1, false);
-    while(true){
-         for(uint32_t i = 0; i<LED_PIN_COUNT; i++){
-             for(uint32_t j = 0; j<LED_PIN_COUNT; j++){
-                 if (i==j){
-                     j++;
-                 }
-                 gpio_set_dir_in_masked(LEDMask);
-                 gpio_set_dir_out_masked((LEDHiMask<<i)&(LEDLowMask<<j));
-                 gpio_put_masked(LEDHiMask<<i, LEDHiMask<<i);
-                 gpio_put_masked(LEDLowMask<<j, ~(LEDLowMask<<j));
-                 printf("hiMask: %x, lowMask: %x, hiResMask: %x \n",LEDHiMask<<i,LEDLowMask<<j,calculate_hi_res_mask(LEDHiMask<<i, LEDLowMask<<j));
-                sleep_ms(100);
-             }
-         }
-    }
-}
