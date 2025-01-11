3995316c5cb9cd8c6bff14d4cde04cabdb7c5733
JSchneid850 <joshua.d.schneider@gmail.com>
2025-01-11 11:16:38 -0600

    Pre 2d array refactor

--- tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/TetrisBusinessCard.cpp
+++ tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/TetrisBusinessCard.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "charlieplexDriver.cpp"
 
 const uint32_t GPIO_PIN_COUNT = 27;
 const uint32_t LED_PIN_COUNT = 22;
@@ -9,21 +10,23 @@ const uint32_t GPIOMask =  0x2FFFFFF;
 
 
 int main() {
-
-    uint32_t LEDTargetBit = 0b1;
-    uint32_t targetMask;
+    charlieplexDriver driver;
+    std::vector<uint32_t> test = {1u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,0u,0u,0u,0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u };
     while(true){
-         for(uint32_t i = 0; i<LED_PIN_COUNT; i++){
-             for(uint32_t j = 0; j<LED_PIN_COUNT; j++){
-                 if (i==j){
-                     j++;
-                 }
-                 targetMask = (LEDTargetBit<<i)|(LEDTargetBit<<j);
-                 gpio_set_dir_in_masked(LEDMask);
-                 gpio_set_dir_out_masked(targetMask);
-                 gpio_put_masked(targetMask, LEDTargetBit<<j);
-                printf("target mask: %x, HiVal: %x\n",targetMask,LEDTargetBit<<j);
-             }
-         }
+    driver.writeFrame(test);
     }
+    // uint32_t LEDTargetBit = 0b1;
+    // uint32_t targetMask;
+    // for(uint32_t i = 0; i<LED_PIN_COUNT; i++){
+    //     for(uint32_t j = 0; j<LED_PIN_COUNT; j++){
+    //         if (i==j){GDB never responded to an interrupt request. Trying to end session anyways
+    //             j++;
+    //         }
+    //         targetMask = (LEDTargetBit<<i)|(LEDTargetBit<<j);
+    //         gpio_set_dir_in_masked(LEDMask);
+    //         gpio_set_dir_out_masked(targetMask);
+    //         gpio_put_masked(targetMask, LEDTargetBit<<j);
+    //         printf("target mask: %x, HiVal: %x\n",targetMask,LEDTargetBit<<j);
+    //     },
+    // }
 }
--- tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/charlieplexDriver.cpp
+++ tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/charlieplexDriver.cpp
@@ -7,15 +7,32 @@ class charlieplexDriver
     private:
         uint32_t GPIO_PIN_COUNT = 27;
         uint32_t LED_PIN_COUNT = 22;
-        uint32_t LEDMask = 0x3FFFFF;
-        uint32_t GPIOMask =  0x2FFFFFF;
+        uint32_t LEDMask = 0xFFFFFF;
+        uint32_t GPIOMask =  0xFFFFFFF;
 
         //need to add index as param for inserting low bit
         uint32_t expandRow(uint32_t row, uint32_t rowIndex){
-            uint32_t shift = (row << 1);
-            uint32_t rightMask = ~(1 << rowIndex);
-            uint32_t leftMask = ~((~0<<rowIndex));
-            return ((shift & rightMask)& ~leftMask)|(leftMask & row);
+            uint32_t shift = (row << 1u);
+            uint32_t rightMask = ~(1u << rowIndex);
+            uint32_t leftMask = ~((~0u<<rowIndex));
+            uint32_t result = ((shift & rightMask)& ~leftMask)|(leftMask & row);
+            return result;
+        }
+
+        //once rev 0.3 is done and I've flipped the led matrix to scan vertically this code will be removed and subsequently burned 
+        int rotateFrameArray(std::vector<uint32_t> &frames){
+            int size = frames.size();
+            std::vector<uint32_t> result(size, 0);
+            
+            for(int i = 0; i < size; i++){
+                for(int j=0; j < size; j++){
+                    if(frames[i] & (1<<j)){// if the j'th bit is set in our original value
+                        result[size - j - 1] + 1u<<j;
+                    }
+                }
+            } 
+            frames = result;
+            return 1;
         }
         
     public:
@@ -25,16 +42,18 @@ class charlieplexDriver
             gpio_init_mask(GPIOMask);
         }
 
-        void writeFrame(const std::vector<uint32_t> &frames){
+        void writeFrame(std::vector<uint32_t> &frames){
             uint32_t framebuff;
-            for(int i = 0; i <= 21; i++){
-                framebuff = expandRow(frames[i], i);
-                printf("Expanded Frame: %x \n Original Frame %x \n");
-                gpio_set_dir_in_masked(LEDMask);
-                gpio_set_dir_out_masked(framebuff | 1<<i);
+            rotateFrameArray(frames);
+            for(uint32_t i = 0u; i < 21u; i++){
+                framebuff = expandRow(frames[i]<<2, i);// shift framebuff to account for pins 1 and 2 in rev0.2 being used for uart debugging
+
+                gpio_set_dir_in_masked(~(framebuff | 1u<<i)); 
+                gpio_set_dir_out_masked(framebuff | 1u<<i);
 
                 //double check what put masked needs to be here
-                gpio_put_masked(framebuff | 1<<i, framebuff);
+                gpio_put_masked(framebuff | 1u<<i, framebuff);
+                framebuff = 0;
             }
         }
-};
+}; 
