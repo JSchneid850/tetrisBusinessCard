ea98f457ad727960752dc37179651c8d369844d4
JSchneid850 <joshua.d.schneider@gmail.com>
2025-01-20 18:36:01 -0600

    Fix driver, now to write tetris

--- tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/TetrisBusinessCard.cpp
+++ tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/TetrisBusinessCard.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cstring>
 #include "pico/stdlib.h"
 #include "charlieplexDriver.cpp"
 
@@ -17,29 +18,76 @@ void uartInit(){
     gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
     uart_init(UART_ID, BAUD_RATE);
 }
+
+void testPatterns(charlieplexDriver &driver) {
+    // Pattern 1: All Off
+    static bool allOff[22][21] = {false};
+
+    // Pattern 2: All On
+    static bool allOn[22][21] = {true};
+
+    // Pattern 3: Single LED
+    static bool singleLed[22][21] = {false};
+    singleLed[0][0] = true;
+
+    // Pattern 4: Last LED
+    static bool lastLed[22][21] = {false};
+    lastLed[21][20] = true;
+
+    // Pattern 5: First Row On
+    static bool firstRowOn[22][21] = {false};
+    for (int j = 0; j < 21; ++j) {
+        firstRowOn[0][j] = true;
+    }
+
+    // Pattern 6: First Column On
+    static bool firstColumnOn[22][21] = {false};
+    for (int i = 0; i < 22; ++i) {
+        firstColumnOn[i][0] = true;
+    }
+
+    // Pattern 7: Checkerboard
+    static bool checkerboard[22][21] = {false};
+    for (int i = 0; i < 22; ++i) {
+        for (int j = 0; j < 21; ++j) {
+            checkerboard[i][j] = (i + j) % 2 == 0;
+        }
+    }
+
+    // Pattern 8: Border Only
+    static bool borderOnly[22][21] = {false};
+    for (int j = 0; j < 21; ++j) {
+        borderOnly[0][j] = true;       // Top border
+        borderOnly[21][j] = true;     // Bottom border
+    }
+    for (int i = 0; i < 22; ++i) {
+        borderOnly[i][0] = true;      // Left border
+        borderOnly[i][20] = true;     // Right border
+    }
+
+    // Array of test patterns
+      bool (*patterns[])[22][21] = {&allOff, &allOn, &singleLed, &lastLed, &firstRowOn, &firstColumnOn, &checkerboard, &borderOnly};
+    const char *patternNames[] = {
+        "All Off",
+        "All On",
+        "Single LED",
+        "Last LED",
+        "First Row On",
+        "First Column On",
+        "Checkerboard",
+        "Border Only"};
+
+    // Display each pattern for 2 seconds
+    for (int i = 0; i < 8; ++i) {
+        printf("Displaying Pattern: %s\n", patternNames[i]);
+        driver.writeFrame(*patterns[i]);
+        sleep_ms(2000); // Display for 2 seconds
+    }
+}
+
 int main() {
     charlieplexDriver driver;
     uartInit();
-    bool test[21][20] = {false};
-    test[0][0] = true;
-    test[20][19] = true;
-    test[0][19] = true;
-    test[20][0] = true;
-    while(true){
-    driver.writeFrame(test);
-    }
-    // uint32_t LEDTargetBit = 0b1;
-    // uint32_t targetMask;
-    // for(uint32_t i = 0; i<LED_PIN_COUNT; i++){
-    //     for(uint32_t j = 0; j<LED_PIN_COUNT; j++){
-    //         if (i==j){GDB never responded to an interrupt request. Trying to end session anyways
-    //             j++;
-    //         }
-    //         targetMask = (LEDTargetBit<<i)|(LEDTargetBit<<j);
-    //         gpio_set_dir_in_masked(LEDMask);
-    //         gpio_set_dir_out_masked(targetMask);
-    //         gpio_put_masked(targetMask, LEDTargetBit<<j);
-    //         printf("target mask: %x, HiVal: %x\n",targetMask,LEDTargetBit<<j);
-    //     },
-    // }
+    testPatterns(driver);
+
 }
--- tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/charlieplexDriver.cpp
+++ tetrisBusinessCard/TetrisBusinessCardCode/TetrisBusinessCard/charlieplexDriver.cpp
@@ -8,10 +8,9 @@ class charlieplexDriver
     private:
         uint32_t GPIO_PIN_COUNT = 27;
         uint32_t LED_PIN_COUNT = 22;
-        uint32_t LEDMask =   0xFFFFFF4;
+        uint32_t LEDMask =   0xFFFFFFc;
         uint32_t GPIOMask =  0xFFFFFFFF;
-        //bool rotateResult[21][21] = {false};
-        uint32_t frameArray[21]= {0}; // a frame with bitmasks equal to vertical bitmask (Will be horizontal bitmask in hardware rev 0.3)
+        uint32_t frameArray[22]= {0}; // a frame with bitmasks equal to vertical bitmask (Will be horizontal bitmask in hardware rev 0.3)
 
         uint32_t expandRow(uint32_t row, uint32_t rowIndex){
             uint32_t shift = (row << 1u);
@@ -21,22 +20,13 @@ class charlieplexDriver
             return result;
         }
 
-        // //once rev 0.3 is done and I've flipped the led matrix to scan vertically this code will be removed and subsequently burned 
-        // void rotateFrameArray(bool (&frame)[21][21]){
-        //     for(int i = 0; i < 21; i++){
-        //         for(int j=0; j < 21; j++){
-        //             rotateResult[20-j][i]= frame[i][j];
-        //         }
-        //     } 
-        // }
-
         //funnily enough we can deal with bool's up to this point, should make it easier to write games just by dealing with an array of on's and off's
-        void compressFrameArray(bool (&frame)[21][20]){
-            for(int i = 0; i <= 21; i++){
+        void compressFrameArray(bool (&frame)[22][21]){
+            for(int i = 0; i < 22; i++){
                 frameArray[i] = 0u;//reset frame array from last call
-                for(uint32_t j = 0; j <= 20; j++){
+                for(uint32_t j = 0; j < 21; j++){
                     if (frame[i][j]){
-                        frameArray[i] = frameArray[i] + 1u << j;
+                        frameArray[i] |= (1u << j);
                     }
                 }
             }
@@ -64,25 +54,16 @@ class charlieplexDriver
             gpio_init_mask(GPIOMask);
         }
 
-        void writeFrame(bool (&frames)[21][20]){
-            //std::cout << "Pre-Rotation" << std::endl;
-            //printArrayState(frames);
-            //rotateFrameArray(frames);
-            //std::cout << "Post-rotation" << std::endl;
-            //printArrayState(rotateResult);
+        void writeFrame(bool (&frames)[22][21]){
+            gpio_set_dir_all_bits(GPIO_IN);//clear last frame
             compressFrameArray(frames);
-            //std::cout << "frameArray" <<std::endl;
-            //printFrameArray();
-            for(uint32_t i = 0u; i <= 21u; i++){
-                uint32_t framebuff = expandRow(frameArray[i]<<2, i<<2);// shift framebuff to account for pins 1 and 2 in rev0.2+ being used for uart debugging
-                //std::cout << "framebuff:" << framebuff << std::endl;
-                gpio_set_dir_in_masked(LEDMask);
-                gpio_set_dir_out_masked((framebuff | 1u<<i<<2));
+            for(uint32_t i = 0u; i < 22u; i++){
+                int shift = 2;// shift framebuff to account for pins 1 and 2 in rev0.2+ being used for uart debugging
+                uint32_t framebuff = expandRow(frameArray[i]<<2, i<<shift);//expand row in frame to be 21 bits long
+                gpio_set_dir_in_masked(~(framebuff | 1u<<i<<shift));//set high-z pins
+                gpio_set_dir_out_masked((framebuff | 1u<<i<<shift));//set pins for IO
 
-                //double check what put masked needs to be here 
-                //1/14/25: this should be fine but need to check row logic since row 0 and 1 may be flipped
-                gpio_put_masked(LEDMask, (~framebuff | 1u<<i<<2));
-                framebuff = 0;
+                gpio_put_masked(LEDMask, (~framebuff | 1u<<i<<shift));//set IO pins for each row
             }
         }
 }; 
